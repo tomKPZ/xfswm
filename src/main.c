5aae0f25cb4bedb06393a479146cb99621eb59c5
tomKPZ <tomKPZ@gmail.com>
2021-04-19 22:16:08 -0700

    add some comments

--- src/main.c
+++ src/main.c
@@ -16,14 +16,17 @@
   } while (0)
 
 int main(void) {
+  // Open a connection to the X server.
   Display* dpy = XOpenDisplay(NULL);
   if (!dpy)
-    return 1;
+    PERROR("XOpenDisplay");
 
+  // Select for CreateNotify events on the root window before the program runs.
   Window root = DefaultRootWindow(dpy);
   XSelectInput(dpy, root, SubstructureNotifyMask);
   XFlush(dpy);
 
+  // Redirect SIGCHLD to a signalfd.
   sigset_t mask;
   sigemptyset(&mask);
   sigaddset(&mask, SIGCHLD);
@@ -33,10 +36,10 @@ int main(void) {
   if (sfd == -1)
     PERROR("signalfd");
 
+  // Run xfswm-init.
   pid_t pid = fork();
-  if (pid < 0) {
+  if (pid < 0)
     PERROR("fork");
-  }
   if (pid == 0) {
     char* const argv[] = {NULL};
     execv("/usr/lib/xfswm-init", argv);
@@ -45,27 +48,25 @@ int main(void) {
 
   int xfd = ConnectionNumber(dpy);
   Window window = None;
-
   while (1) {
-    XFlush(dpy);
-
+    // Wait for sfd or xfd to become readable.
     fd_set in_fds;
     FD_ZERO(&in_fds);
     FD_SET(xfd, &in_fds);
     FD_SET(sfd, &in_fds);
     int max_fd = sfd > xfd ? sfd : xfd;
     int ret = select(max_fd + 1, &in_fds, NULL, NULL, NULL);
-    if (ret == -1) {
-      if (errno != EINTR) {
-        PERROR("select");
-        break;
-      }
-    }
+    if (ret == -1 && errno != EINTR)
+      PERROR("select");
     if (ret <= 0)
       continue;
+
+    // Check if an X event is available.
     if (FD_ISSET(xfd, &in_fds) && XEventsQueued(dpy, QueuedAfterReading)) {
       XEvent event;
       XNextEvent(dpy, &event);
+
+      // Check for the first window creation.
       if (event.type == CreateNotify && window == None) {
         window = event.xcreatewindow.window;
         XSelectInput(dpy, root, StructureNotifyMask);
@@ -77,14 +78,19 @@ int main(void) {
                          &depth)) {
           XMoveResizeWindow(dpy, window, 0, 0, width, height);
         }
-      } else if (event.type == ConfigureNotify &&
-                 event.xconfigure.window == root) {
+      }
+
+      // Check for a root window resize.
+      if (event.type == ConfigureNotify && event.xconfigure.window == root) {
         assert(window != None);
         XMoveResizeWindow(dpy, window, 0, 0, event.xconfigure.width,
                           event.xconfigure.height);
       }
+
       XFlush(dpy);
     }
+
+    // Check if a child has exited.
     if (FD_ISSET(sfd, &in_fds)) {
       struct signalfd_siginfo fdsi;
       ssize_t s = read(sfd, &fdsi, sizeof(fdsi));
