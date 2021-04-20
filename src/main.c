40fe5667640980597869944ad6a9c98a3ba45ce2
tomKPZ <tomKPZ@gmail.com>
2021-04-19 21:56:45 -0700

    exit when child exits

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <X11/Xlib.h>
+#include <assert.h>
 #include <errno.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/signalfd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
+#define PERROR(msg)     \
+  do {                  \
+    perror(msg);        \
+    exit(EXIT_FAILURE); \
+  } while (0)
 
 int main(void) {
   Display* dpy = XOpenDisplay(NULL);
@@ -10,48 +22,82 @@ int main(void) {
 
   Window root = DefaultRootWindow(dpy);
   XSelectInput(dpy, root, SubstructureNotifyMask);
+  XFlush(dpy);
 
-  Screen* screen = DefaultScreenOfDisplay(dpy);
-  int fd = ConnectionNumber(dpy);
+  sigset_t mask;
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGCHLD);
+  if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1)
+    PERROR("sigprocmask");
+  int sfd = signalfd(-1, &mask, 0);
+  if (sfd == -1)
+    PERROR("signalfd");
 
   pid_t pid = fork();
   if (pid < 0) {
-    perror("fork");
-    return 1;
+    PERROR("fork");
   }
   if (pid == 0) {
     char* const argv[] = {NULL};
     execv("/usr/lib/xfswm-init", argv);
-    perror("execv");
-    return 1;
+    PERROR("execv");
   }
+
+  int xfd = ConnectionNumber(dpy);
+  Window window = None;
 
   while (1) {
     XFlush(dpy);
 
     fd_set in_fds;
     FD_ZERO(&in_fds);
-    FD_SET(fd, &in_fds);
-    int ret = select(fd + 1, &in_fds, NULL, NULL, NULL);
+    FD_SET(xfd, &in_fds);
+    FD_SET(sfd, &in_fds);
+    int max_fd = sfd > xfd ? sfd : xfd;
+    int ret = select(max_fd + 1, &in_fds, NULL, NULL, NULL);
     if (ret == -1) {
       if (errno != EINTR) {
-        perror("select");
-        goto cleanup;
+        PERROR("select");
+        break;
       }
-    } else if (ret > 0) {
-      if (XPending(dpy)) {
-        XEvent event;
-        XNextEvent(dpy, &event);
-        if (event.type == CreateNotify) {
-          Window window = event.xcreatewindow.window;
-          XMoveResizeWindow(dpy, window, 0, 0, WidthOfScreen(screen),
-                            HeightOfScreen(screen));
+    }
+    if (ret <= 0)
+      continue;
+    if (FD_ISSET(xfd, &in_fds) && XEventsQueued(dpy, QueuedAfterReading)) {
+      XEvent event;
+      XNextEvent(dpy, &event);
+      if (event.type == CreateNotify && window == None) {
+        window = event.xcreatewindow.window;
+        XSelectInput(dpy, root, StructureNotifyMask);
+
+        Window root_ret;
+        int x, y;
+        unsigned int width, height, border, depth;
+        if (XGetGeometry(dpy, root, &root_ret, &x, &y, &width, &height, &border,
+                         &depth)) {
+          XMoveResizeWindow(dpy, window, 0, 0, width, height);
         }
+      } else if (event.type == ConfigureNotify &&
+                 event.xconfigure.window == root) {
+        assert(window != None);
+        XMoveResizeWindow(dpy, window, 0, 0, event.xconfigure.width,
+                          event.xconfigure.height);
       }
+      XFlush(dpy);
+    }
+    if (FD_ISSET(sfd, &in_fds)) {
+      struct signalfd_siginfo fdsi;
+      ssize_t s = read(sfd, &fdsi, sizeof(fdsi));
+      if (s != sizeof(fdsi))
+        PERROR("read");
+      pid_t waited = waitpid(-1, NULL, WNOHANG);
+      if (waited == -1)
+        PERROR("waitpid");
+      if (waited == pid)
+        break;
     }
   }
 
-cleanup:
   XCloseDisplay(dpy);
-  return 0;
+  return EXIT_SUCCESS;
 }
